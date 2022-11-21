9f0988b57babe84131a7d5490e021e1bb6634514
Sunil Kumar <sonuame@aol.com>
2022-11-21 23:49:24 +0530

    Converted to windows

---
+++ BatteryMonitorService/Program.cs
@@ -0,0 +1,48 @@
+﻿using LoggerLite.EventLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatteryMonitorService
+{
+    internal class Program
+    {
+        static EventLogLogger logger = new EventLogLogger("Battery-Logger", "Monitor");
+
+        static void Main(string[] args)
+        {
+            StartMonitoring().Wait();
+        }
+
+        
+
+        async static Task StartMonitoring()
+        {
+
+            var query = new ObjectQuery("Select * FROM Win32_Battery");
+            var searcher = new ManagementObjectSearcher(query);
+            var collection = searcher.Get();
+
+            var battery = collection.OfType<ManagementObject>().FirstOrDefault();
+            var percentage = (UInt16)battery.GetPropertyValue("EstimatedChargeRemaining");
+            var status = (UInt16)battery.GetPropertyValue("BatteryStatus");
+
+            Console.WriteLine($"Status {status}, Level {percentage}%");
+            logger.LogInfo($"Status {(status == 2 ? "Charging" : "On Battery")}, Level {percentage}%");
+
+            if (status == 1)
+            {
+                logger.LogError($"Status {(status == 2 ? "Charging" : "On Battery")}, Level {percentage}%");
+            }
+
+
+
+            await Task.Delay(2 * 60 * 1000);
+            await StartMonitoring();
+        }
+
+    }
+}
